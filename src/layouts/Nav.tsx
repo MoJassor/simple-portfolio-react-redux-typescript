e3a453a1467110321a939eb98624e1f7f3fa09cd
Mohammad Jassor <mohammadjassor>
2022-09-20 18:29:51 +0300

    add Nav component and it's styles

---
+++ src/layouts/Nav.tsx
@@ -0,0 +1,27 @@
+import NavLinkItem from "../components/navLink";
+import "./Nav.css";
+import ThemeToggle from "../components/themeToggle";
+import { useAppSelector } from "../app/hooks";
+import { selectTheme } from "../features/theme";
+
+const Nav = () => {
+  const theme = useAppSelector(selectTheme);
+  return (
+    <header style={{ backgroundColor: theme.backgroundColor }}>
+      <nav>
+        <div className="header">
+          <NavLinkItem path="/" isIcon={true} icon="bi bi-house-fill" />
+          <NavLinkItem path="/about" isIcon={false} content="About" />
+          <NavLinkItem path="/work" isIcon={false} content="Work" />
+          <NavLinkItem path="/project" isIcon={false} content="projects" />
+          <NavLinkItem path="/contact" isIcon={false} content="Contact" />
+        </div>
+        <div style={{ display: "flex" }}>
+          <ThemeToggle />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
