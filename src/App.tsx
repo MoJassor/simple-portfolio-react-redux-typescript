a3ae3e0f5e29455a09f2fcfa1d283153cfe57578
Mohammad Jassor <mohammadjassor>
2022-09-20 18:24:08 +0300

    fix some imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Layout from "./layouts/layout";
+import Home from "./pages/Home";
+import Layout from "./layouts";
 import "./App.css";
 function App() {
   return (
